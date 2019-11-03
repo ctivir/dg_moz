32e8c2216892241aebc67a7757268596fd4d00ca
ctivir <ctivir@gmail.com>
2019-11-03 23:22:04 +0200

    Add new features (comments)

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,16 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    #image = mod
-    text = models.TextField()
+    title = models.CharField(max_length=255, verbose_name="Titulo")
+    slug = models.SlugField(unique=True, max_length=255)
+    image = models.ImageField(default="default.png", verbose_name="Foto")
+    text = models.TextField(verbose_name="Post")
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(upload_to='media')
+
+    class Meta:
+        ordering = ['-created_date']
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +21,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField(verbose_name="Comentario")
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
